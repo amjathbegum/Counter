a6b6771e41060395fa7e3bd4bf298731f9f40161
amjathbegum <hamjathbegum@gmail.com>
2025-04-29 11:08:13 +0530

    add to decreses

--- counter.js
+++ counter.js
@@ -4,32 +4,36 @@ const resetBtn = document.getElementById("resetBtn");
 
 let count = 0;
 
-increseBtn.onclick =  function(){
+
+increseBtn.onclick = function () {
   count++;
   countLabel.textContent = count;
 
+  increseBtn.addEventListener('click', function () {
+    decreseBtn.style.display = 'inline-block';
+  });
 }
 
-resetBtn.onclick = function(){
+resetBtn.onclick = function () {
   count = 0;
   countLabel.textContent = count;
 }
 
-decreseBtn.onclick = function() {
-  if(count >0){
-   count--;
-   countLabel.textContent = count;
-}
-else{
- var x = document.getElementById("decreseBtn");
-   if(x.style.display === "none"){
-     x.style.display = "block";
-   }
-   else {
-    x.style.display = "none";
-   }
+decreseBtn.onclick = function () {
+  if (count > 0) {
+    count--;
+    countLabel.textContent = count;
+  }
+  else {
+    var x = document.getElementById("decreseBtn");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    }
+    else {
+      x.style.display = "none";
+    }
 
-}
+  }
 }
 
 
