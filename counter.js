bc8db724885d3115f3c87693e7832e939c7bf8eb
amjathbegum <hamjathbegum@gmail.com>
2025-05-15 12:43:50 +0530

    to count stop 20 show the time

--- counter.js
+++ counter.js
@@ -3,16 +3,29 @@ const increseBtn = document.getElementById("increseBtn");
 const resetBtn = document.getElementById("resetBtn");
 
 let count = 0;
+var t;
+
+/*
+function myTimer(){
+  const d = new Date();
+  document.getElementById(increseBtn).innerHTML = d.toLocaleTimeString();
+}
+*/
 
 
-increseBtn.onclick = function () {
-  count++;
+increseBtn.addEventListener('click' ,function () {
+  if(count<20){
+      count++; 
+  }
+  else if (count == count){
+    const d = new Date();
+    document.getElementById("demo").innerHTML = d.toLocaleTimeString();
+  }
   countLabel.textContent = count;
-
   increseBtn.addEventListener('click', function () {
     decreseBtn.style.display = 'inline-block';
   });
-}
+});
 
 resetBtn.onclick = function () {
   count = 0;
