babf254a554c62e6b1d5d8c3b5d2401bb79bfcae
amjathbegum <hamjathbegum@gmail.com>
2025-05-16 15:32:44 +0530

    set timer when stop the count 20

--- counter.js
+++ counter.js
@@ -1,6 +1,9 @@
+
 const decreseBtn = document.getElementById("decreseBtn");
 const increseBtn = document.getElementById("increseBtn");
 const resetBtn = document.getElementById("resetBtn");
+
+
 
 let count = 0;
 var t;
@@ -11,15 +14,25 @@ function myTimer(){
   document.getElementById(increseBtn).innerHTML = d.toLocaleTimeString();
 }
 */
-
+  
 
 increseBtn.addEventListener('click' ,function () {
   if(count<20){
       count++; 
   }
-  else if (count == count){
-    const d = new Date();
-    document.getElementById("demo").innerHTML = d.toLocaleTimeString();
+  
+   else if (count==count){
+      let hrs = document.getElementById("hrs");
+     let min =document.getElementById("min");
+     let sec = document.getElementById("sec");
+
+     setInterval(()=>{
+      let currenttime =new Date();
+
+      hrs.innerHTML=(currenttime.getHours()<10?"0":"") + currenttime.getHours();
+      min.innerHTML=(currenttime.getMinutes()<10?"0":"") + currenttime.getMinutes();
+      sec.innerHTML=(currenttime.getSeconds()<10?"0":"") + currenttime.getSeconds();
+     },1000)
   }
   countLabel.textContent = count;
   increseBtn.addEventListener('click', function () {
@@ -48,5 +61,4 @@ decreseBtn.onclick = function () {
 
   }
 }
-
 
